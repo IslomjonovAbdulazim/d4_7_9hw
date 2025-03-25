704a1d4dea3b9996b386809bd2cc901a518d3c25
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-03-25 09:20:12 +0500

    imp

--- lib/pages/start_page.dart
+++ lib/pages/start_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class StartPage extends StatelessWidget {
   const StartPage({super.key});
@@ -6,6 +7,7 @@ class StartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
@@ -16,8 +18,109 @@ class StartPage extends StatelessWidget {
               // Address
 
               // *Order Type
+              SizedBox(
+                height: 60,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                        decoration: BoxDecoration(
+                          color: Color(0xffF2F2F2),
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        child: Text(
+                          "Department",
+                          style: GoogleFonts.roboto(
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 8),
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                        decoration: BoxDecoration(
+                          color: Color(0xff743D73),
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        child: Text(
+                          "Nation",
+                          style: GoogleFonts.roboto(
+                            fontSize: 18,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 8),
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                        decoration: BoxDecoration(
+                          color: Color(0xffF2F2F2),
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        child: Text(
+                          "International",
+                          style: GoogleFonts.roboto(
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
 
               // *Location
+              SizedBox(height: 15),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                decoration: BoxDecoration(
+                  color: Color(0xffF2F2F2),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          height: 20,
+                          width: 20,
+                          decoration: BoxDecoration(
+                            color: Color(0xff743D73),
+                            shape: BoxShape.circle,
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Text("Pick up from"),
+                        Spacer(),
+                        Text("Bogota"),
+                        SizedBox(width: 10),
+                        Icon(Icons.chevron_right),
+                      ],
+                    ),
+                    SizedBox(height: 15),
+                    Row(
+                      children: [
+                        Container(
+                          height: 20,
+                          width: 20,
+                          decoration: BoxDecoration(
+                            color: Color(0xffFE6F23),
+                            shape: BoxShape.circle,
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Text("Delivery To"),
+                        Spacer(),
+                        Text("Texas"),
+                        SizedBox(width: 10),
+                        Icon(Icons.chevron_right),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
 
               // Type Of Service
 
